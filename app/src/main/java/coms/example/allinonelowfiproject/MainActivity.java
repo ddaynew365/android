b9ed8154ed7bf31f4b4ec4adc61eb72e8b2e5adb
JMcunst <xnslqjtmghaz@naver.com>
2021-03-16 19:10:50 +0900

    feat : func applied sliding root navigation(srn)

--- app/src/main/java/coms/example/allinonelowfiproject/MainActivity.java
+++ app/src/main/java/coms/example/allinonelowfiproject/MainActivity.java
@@ -1,14 +1,28 @@
 package coms.example.allinonelowfiproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.Color;
 import android.os.Bundle;
+
+import coms.example.allinonelowfiproject.slidingRootNav.SlidingRootNavBuilder;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_page);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+
+        new SlidingRootNavBuilder(this)
+                .withMenuLayout(R.layout.main_sliding_menu)
+                .withToolbarMenuToggle(toolbar)
+                .inject();
     }
 }
