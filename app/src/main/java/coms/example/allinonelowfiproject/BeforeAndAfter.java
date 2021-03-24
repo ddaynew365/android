c89eb74d2ca12d73ad022c23073458db68fe66d6
Yegeurina <uhoo@ajou.ac.kr>
2021-03-25 00:04:33 +0900

    Add BeforeAndAfter Test

--- app/src/main/java/coms/example/allinonelowfiproject/BeforeAndAfter.java
+++ app/src/main/java/coms/example/allinonelowfiproject/BeforeAndAfter.java
@@ -10,5 +10,6 @@ public class BeforeAndAfter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_before_and_after);
+        //test
     }
 }
